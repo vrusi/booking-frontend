f62866e5d4c954e4f6baddad04f7ac04b8487f79
vrusi <me@vrusinkova.com>
2021-04-16 02:16:46 +0200

    Make connect to server via android emulator

--- booking/lib/api/api_client.dart
+++ booking/lib/api/api_client.dart
@@ -6,7 +6,7 @@ class Api extends http.BaseClient {
   final http.Client _inner = http.Client();
   final String baseUrl;
 
-  Api({this.baseUrl = "localhost:8000"});
+  Api({this.baseUrl = "10.0.2.2:8000"});
 
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) {
